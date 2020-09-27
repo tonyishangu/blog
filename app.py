d0853015308d36a08f57a72dd5b53fb2c3e9a976
tonyishangu <tonyishangu@gmail.com>
2020-09-27 23:04:05 +0300

    add database

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask,request,render_template,redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
