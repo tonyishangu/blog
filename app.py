301cc7e79ce5ce0c54374fb3fba35d612a9f491f
tonyishangu <tonyishangu@gmail.com>
2020-09-27 22:33:05 +0300

    add post route

--- app.py
+++ app.py
@@ -6,5 +6,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/posts', methods=['GET', 'POST'])
+def posts():
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('post.html', posts=all_posts)
+
 if __name__ == "__main__":
     app.run(debug=True)
